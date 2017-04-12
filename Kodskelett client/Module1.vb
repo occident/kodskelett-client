68e109fd488ffbf372a3b40c30be47473efefaf3
Daniel Treviño <daniel.trevino@occident.se>
2017-04-12 15:12:40 +0200

    Added an advanced getaip method example

--- Kodskelett client/Module1.vb
+++ Kodskelett client/Module1.vb
@@ -5,9 +5,13 @@ Module Module1
 		'--- Creating the settings Object
 		Dim oSettings As New Settings("https://theendpoint.com/Soap/ExternalEarkivDMZ.svc/Soap", "C:\certificate.pfx", "certPassword", "192.168.1.1")
 
-		'--- Creating the client Object
-		Dim oClient As New Client(oSettings)
+		'--- Creating a custom logger file
+		Dim oExecutionResults As New Logger("C:\Output", "execution.txt")
 
+		'--- Creating the client Object
+		Dim oClient As New Client(oSettings, oExecutionResults)
+
+		
 		Console.WriteLine("Executing...")
 
 		oClient.GetAip(1415422, "C:\Output")
