1bf8000aeef57207de8a4d070d5f57287e73ba0d
Daniel Treviño <daniel.trevino@occident.se>
2017-04-12 15:24:38 +0200

    Adding an advanced example on the getaip method

--- Kodskelett client/Module1.vb
+++ Kodskelett client/Module1.vb
@@ -5,16 +5,27 @@ Module Module1
 		'--- Creating the settings Object
 		Dim oSettings As New Settings("https://theendpoint.com/Soap/ExternalEarkivDMZ.svc/Soap", "C:\certificate.pfx", "certPassword", "192.168.1.1")
 
+		'--- Enable advanced log file to see what exactly happens on the background
+		oSettings.advancedLog = True
+
 		'--- Creating a custom logger file
 		Dim oExecutionResults As New Logger("C:\Output", "execution.txt")
 
 		'--- Creating the client Object
 		Dim oClient As New Client(oSettings, oExecutionResults)
 
-		
 		Console.WriteLine("Executing...")
 
-		oClient.GetAip(1415422, "C:\Output")
+		Dim oResponseObject As SSA.ArchiveObject = oClient.GetAipAsObject(123456).ArchiveObject
+
+		oExecutionResults.Log("Display Name:" & oResponseObject.DisplayName)
+		oExecutionResults.Log("Object type:" & oResponseObject.ObjectType)
+
+		oExecutionResults.Log("The items of this object are: ")
+		For Each i In oResponseObject.Items
+			oExecutionResults.Log("Display Name:" & i.DisplayName)
+			oExecutionResults.Log("Object type:" & i.ObjectType)
+		Next
 
 		Console.WriteLine("Finished the execution")
 		Console.ReadLine()
