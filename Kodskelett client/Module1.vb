e92d7673f45d65da848a69e40587c536d4de8197
Daniel Treviño <daniel.trevino@occident.se>
2017-04-12 14:34:06 +0200

    Basic getaip example added

--- Kodskelett client/Module1.vb
+++ Kodskelett client/Module1.vb
@@ -2,7 +2,19 @@
 Module Module1
 
 	Sub Main()
-		
+		'--- Creating the settings Object
+		Dim oSettings As New Settings("https://theendpoint.com/Soap/ExternalEarkivDMZ.svc/Soap", "C:\certificate.pfx", "certPassword", "192.168.1.1")
+
+		'--- Creating the client Object
+		Dim oClient As New Client(oSettings)
+
+		Console.WriteLine("Executing...")
+
+		oClient.GetAip(1415422, "C:\Output")
+
+		Console.WriteLine("Finished the execution")
+		Console.ReadLine()
+
 	End Sub
 
 End Module
